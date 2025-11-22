2a37a625ee6964c8b64f5126f1be5de4f905e834
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2025-11-22 22:13:06 +0000

    Implementa o componente de sidebar com navegação e estilo

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    MatListModule,
+    MatIconModule,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  menuItems = [
+    { label: 'Cursos', icon: 'school', route: '/cursos' },
+    { label: 'Turmas', icon: 'groups', route: '/turmas' }, // Exemplo futuro
+    { label: 'Alunos', icon: 'person', route: '/alunos' }, // Exemplo futuro
+  ];
 }
