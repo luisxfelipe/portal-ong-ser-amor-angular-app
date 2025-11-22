d6fd20e485b64c649095b8ef7da6f55ef658714d
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2025-11-22 03:09:27 +0000

    implementação um interceptor de autenticação para gerenciar tokens de autorização em requisições HTTP.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { CookieService } from 'ngx-cookie-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    CookieService,
   ],
 };
