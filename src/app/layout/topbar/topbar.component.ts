5f9da43a299e4180327b42939975ba3cdc353eb1
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2025-11-23 00:02:57 +0000

    Implementa o componente de topbar

--- src/app/layout/topbar/topbar.component.ts
+++ src/app/layout/topbar/topbar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.scss'
+  styleUrl: './topbar.component.scss',
 })
 export class TopbarComponent {
+  authService = inject(AuthService);
 
+  logout() {
+    this.authService.logout();
+  }
 }
